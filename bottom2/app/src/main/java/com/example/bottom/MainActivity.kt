6228fabc9f691c5c7566474462e908bf834fe412
p1pc1 <aligohargazi644@gmail.com>
2023-06-11 12:36:42 +0430

    project

---
+++ bottom2/app/src/main/java/com/example/bottom/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.bottom
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.bottom.ui.theme.BottomNavigationsTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BottomNavigationsTheme() {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(){
+    val navController = rememberNavController()
+    Scaffold(bottomBar = { MyBottomNavigation(navController = navController)}) {
+        Box(modifier = Modifier.padding(it)){
+            NavHost(navController = navController, startDestination = Login.Route ){
+                composable(Login.Route){
+                    login(navController)
+                }
+                composable(Welcome.Route){
+                    Welcome
+                }
+            }
+        }
+    }
+}
---
+++ bottom2/app/src/main/java/com/example/bottom/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.bottom.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFDBFF3B)
+val Purple500 = Color(0xFFEE3000)
+val Purple700 = Color(0xFF00A4B3)
+val Teal200 = Color(0xFFDA9D03)
